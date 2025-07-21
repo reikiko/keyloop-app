efa282bc110168a6b66f7c0e04437d445d9dcbf5
reikiko <muhammad.mk901@gmail.com>
2025-07-21 19:03:06 +0700

    hotfix: suspense boundary

--- client/src/app/(nondashboard)/search/page.tsx
+++ client/src/app/(nondashboard)/search/page.tsx
@@ -3,7 +3,7 @@
 import { NAVBAR_HEIGHT } from "@/lib/constants";
 import { useAppDispatch, useAppSelector } from "@/state/redux";
 import { useSearchParams } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, Suspense } from "react";
 import FiltersBar from "./FiltersBar";
 import FiltersFull from "./FiltersFull";
 import { cleanParams } from "@/lib/utils";
@@ -36,32 +36,34 @@ const SearchPage = () => {
 
     const cleanedFilters = cleanParams(initialFilters);
     dispatch(setFilters(cleanedFilters));
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <div
-      className="w-full mx-auto px-5 flex flex-col"
-      style={{
-        height: `calc(100vh - ${NAVBAR_HEIGHT}px)`,
-      }}
-    >
-      <FiltersBar />
-      <div className="flex justify-between flex-1 overflow-hidden gap-3 mb-5">
-        <div
-          className={`h-full overflow-auto transition-all duration-300 ease-in-out ${
-            isFiltersFullOpen
-              ? "w-3/12 opacity-100 visible"
-              : "w-0 opacity-0 invisible"
-          }`}
-        >
-          <FiltersFull />
-        </div>
-        <Map />
-        <div className="basis-4/12 overflow-y-auto">
-          <Listings />
+    <Suspense>
+      <div
+        className="w-full mx-auto px-5 flex flex-col"
+        style={{
+          height: `calc(100vh - ${NAVBAR_HEIGHT}px)`,
+        }}
+      >
+        <FiltersBar />
+        <div className="flex justify-between flex-1 overflow-hidden gap-3 mb-5">
+          <div
+            className={`h-full overflow-auto transition-all duration-300 ease-in-out ${
+              isFiltersFullOpen
+                ? "w-3/12 opacity-100 visible"
+                : "w-0 opacity-0 invisible"
+            }`}
+          >
+            <FiltersFull />
+          </div>
+          <Map />
+          <div className="basis-4/12 overflow-y-auto">
+            <Listings />
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
